4ce4dbefa7a2f87b2dec4a4a5266469bec3e2c3f
Mohanapriyan M <152837571+mohanapriyan2006@users.noreply.github.com>
2025-06-15 17:08:01 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
         <h5>m<i>ews</i></h5>
-        <p>&copy; 2024 : Developed by <b>Mohanapriyan.M</b>
+        <p>&copy; 2024 : Developed by<a target="new" href="https://mohanapriyan.netlify.app/"><b>Mohanapriyan.M</b></p></a>
         </p>
     </div>
   )
